bd0fff44619fcd3637b615d1d08aa03f64de88d1
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2023-11-16 14:16:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 console.log("yosss")
 
 boxes = document.querySelectorAll(".bone-box")
+var oldPos = 5
 
 score = 0
 
 
+
 function moveBone() {
-    newBox = Math.floor(Math.random() * 9) + 1
 
+    document.querySelector(".has-bone").classList.remove("has-bone")
 
+    let box  = Math.floor(Math.random() * 9) + 1
+    
+    console.log(box)
+    boxes[box-1].classList.add("has-bone")
+    oldPos = box
 }
+
+// setInterval(moveBone, 700)
