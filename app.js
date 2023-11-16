4bb2f1fca33c253bb4464466ce42a462d2f923d4
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2023-11-16 14:50:20 +0530

    aa

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 console.log("yosss")
 
 boxes = document.querySelectorAll(".bone-box")
+var scoreText = document.getElementById("score")
 var oldPos = 5
+var score = 0
+var clicked = false
 
-score = 0
-
-
+let boneChanging = 700
 
 function moveBone() {
 
@@ -16,16 +17,27 @@ function moveBone() {
         box  = Math.floor(Math.random() * 9) + 1
     }
     boxes[box-1].classList.add("has-bone")
+    clicked = false
     oldPos = box
 }
 
-setInterval(moveBone, 700)
+let interval = setInterval(moveBone, boneChanging)
 
 
 boxes.forEach(box => {
     box.addEventListener("click", () =>{
-        if(box.classList.contains("has-bone")){
-            
+        if(box.classList.contains("has-bone") && clicked == false){
+            score++
+            if(score%15 == 0 && boneChanging>240){
+                boneChanging -= 40  //the bone change spositon 50ms faster every 15 time clicked upon....for graduall hardering diffuclty
+                console.log(boneChanging)
+                clearInterval(interval)
+                interval = setInterval(moveBone, boneChanging)
+            } 
+            scoreText.textContent = "Score: "+score
+            clicked = true
+            console.log(score)
         }
     })
 });
+
